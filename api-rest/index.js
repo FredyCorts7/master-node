60477997d887844aabe267e4417a774e0fb85112
fredycorts7 <fred.cor.14@gmail.com>
2021-04-06 14:45:33 -0500

    [ADD]: getting and handling payload

--- api-rest/index.js
+++ api-rest/index.js
@@ -1,5 +1,6 @@
 // Dependencies
 const http = require('http');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // Server should respond to all requests
 const server = http.createServer(function (req, res) {
@@ -24,13 +25,23 @@ const server = http.createServer(function (req, res) {
   // Get the headers as an object
   const headers = req.headers;
 
-  // Send the response
-  res.end('Hello World\n');
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
 
-  // Log the request path
-  console.log(
-    `Request received with these headers: ${JSON.stringify(headers, null, 2)}`
-  );
+  req.on('data', function (data) {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', function () {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end('Hello World\n');
+
+    // Log the request path
+    console.log(`Request received with this payload: ${buffer}`);
+  });
 });
 
 // Start server, and have it listen on port
